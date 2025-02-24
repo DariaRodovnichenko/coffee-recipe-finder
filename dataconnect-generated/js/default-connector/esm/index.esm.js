fa4dfa69b201b7f40c7ca95ae2bbce486896ccfc
DariaRodovnichenko <125218414+DariaRodovnichenko@users.noreply.github.com>
2025-02-24 12:55:40 +0100

    Removed .vscode, dataconnect, and dataconnect-generated from Git tracking

--- dataconnect-generated/js/default-connector/esm/index.esm.js
+++
@@ -1,8 +0,0 @@
-import { , validateArgs } from 'firebase/data-connect';
-
-export const connectorConfig = {
-  connector: 'default',
-  service: 'coffee-recipe-finder',
-  location: 'us-central1'
-};
-
